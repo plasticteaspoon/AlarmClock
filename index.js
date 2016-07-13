5faab8aef3db1ca2dbbda06546c7304a73cb3a1d
jessica drury <jessica@raspberrypi>
2016-07-13 18:42:32 +0100

    fixed the log page

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 var omxplayer;
 
 app.use(express.static(__dirname + '/html'));
-
+ 
 var insertLog = function (entry) {
     entry.date = new Date();
     db.insert(entry, function (err) {
@@ -34,28 +34,6 @@ var playSound = function (fileName, balance) {
 
 app.get('/', function (request, response) {
     response.sendFile(__dirname + '/html/home.html');
-});
-
-app.get('/school-week', function (request, response) {
-    exec('crontab /home/jessica/AlarmClock/Express/school-week.cron');
-    response.send('School alarms are go!!!');
-});
-
-app.get('/no-alarm', function (request, response) {
-    exec('crontab -r');
-    response.send('School alarms are gone!!!');
-});
-
-app.get('/crontab', function (request, response) {
-    exec('crontab -l', function (error, stdout, stderr) {
-        response.set('Content-Type', 'text/plain');
-        response.send(stdout);
-    });
-});
-
-app.get('/api/myName', function (request, response) {
-    response.set('Content-Type', 'application/json');
-    response.send(JSON.stringify({ 'name' : 'Roger' }));
 });
 
 app.get('/api/crontab', function (request, response) {
@@ -108,7 +86,12 @@ app.post('/api/killOmx', function (request, response) {
     response.send();
 });
 
-
+app.get('/api/getLogs', function (request, response) {
+    db.find({}, function (error, data) {
+        response.set('Content-Type', 'application/json');
+        response.send(JSON.stringify(data));
+    });
+});
 
 app.listen(8080, function () {
     console.log('server started');
